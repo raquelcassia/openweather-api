c58281f091dd9bac35e4649871f629a4c83f6190
raquelcassia <rcassia_scarvalho@hotmail.com>
2021-07-12 10:21:01 -0300

    Cod 404 e 401 - Steps de Teste de Dados Meteorológicos Atuais
    Cod 401:
    Pesquisa de Api Key Inválida
    Cod 404:
    Pesquisa Por Nome de Cidade Inválida
    Pesquisa de Cidade Por ID Inválido
    Pesquisa de Cidade Por Coordenadas Geográficas Inválidas
    Pesquisa de Cidade Por Zip Code Inválido

--- cypress/integration/CurrentWeatherData.js
+++ cypress/integration/CurrentWeatherData.js
@@ -5,47 +5,53 @@
 
 // API_KEY
 
-/* describe("Pesquisa de Api Key Inválida", () => {
+ describe("Pesquisa de Api Key Inválida", () => {
     it("GET -READ", () => {
-        cy.request(
-            "GET",
-            "weather?q=" + Cypress.env("CIDADE") + "&appid=" + Cypress.env("API_KEY")).then((response) => {
-                //expect(response.status).to.have.property(401),
-                //expect(response.body).to.have.property("message", "Invalid API key. Please see http://openweathermap.org/faq#error401 for more info.")
+        cy.request({
+            method: "GET",
+            url: "weather?q=Inexistente&appid=ebd69e528d958b2666b3d73a19cedcbc",
+            failOnStatusCode: false
+        }).then((response) => {
+            cy.log(JSON.stringify(response.body));
+            expect(response.body).to.have.property("cod", 401),
+            expect(response.body).to.have.property("message", "Invalid API key. Please see http://openweathermap.org/faq#error401 for more info.")
 
             })
     })
-}) */
+}) 
 
 // POR NOME DE CIDADE
 
 describe("Pesquisa Por Nome de Cidade Válida", () => {
-    it ("GET -READ", () => {
-        cy.request("GET", "weather?q=London&appid=" 
-        + Cypress.env("API_KEY")).then((response) => {
-            
-            cy.log(JSON.stringify(response.body));
-            expect(response).to.have.property("status", 200)
-            expect(response.body).to.have.property("name", "London")
-            expect(response.body.sys).to.have.property("country", "GB")
-            expect(response.body).to.have.property("id", 2643743)
-        })
+    it("GET -READ", () => {
+        cy.request("GET", "weather?q=London&appid="
+            + Cypress.env("API_KEY")).then((response) => {
+
+                cy.log(JSON.stringify(response.body));
+                expect(response).to.have.property("status", 200)
+                expect(response.body).to.have.property("name", "London")
+                expect(response.body.sys).to.have.property("country", "GB")
+                expect(response.body).to.have.property("id", 2643743)
+            })
     })
 })
 
 
-/* describe("Pesquisa Por Nome de Cidade Inválida", () => {
+ describe("Pesquisa Por Nome de Cidade Inválida", () => {
     it("GET -READ", () => {
-        cy.request("GET", "weather?q=Inexistente&appid="
-        + Cypress.env("API_KEY")).then((response) => {
+        cy.request({
+            method: "GET", 
+            url: "weather?q=Inexistente&appid="
+            + Cypress.env("API_KEY"), 
+            failOnStatusCode: false}) .then((response) => {
 
             cy.log(JSON.stringify(response.body));
-            //expect(response).to.have.property("cod", 404)
-            //expect(response.body).to.have.property("message", "city not found")
+            expect(response.body).to.have.property("cod", "404")
+            expect(response.body).to.have.property("message", "city not found")
 
         })
     })
-}) */
+}) 
 
 
 // POR ID DE CIDADE (city.list.json)
@@ -54,53 +60,38 @@ describe("Pesquisa Por Nome de Cidade Válida", () => {
 describe("Pesquisa de Cidade Por ID Válido", () => {
     it("GET -READ", () => {
         cy.request("GET", "api.openweathermap.org/data/2.5/weather?id=1850147&appid="
-        + Cypress.env("API_KEY")).then((response) => {
+            + Cypress.env("API_KEY")).then((response) => {
 
-            cy.log(JSON.stringify(response.body));
-            expect(response).to.have.property("status", 200)
-            expect(response.body).to.have.property("name", "Tokyo")
-            expect(response.body.sys).to.have.property("country", "JP")
-            expect(response.body).to.have.property("id", 1850147)
+                cy.log(JSON.stringify(response.body));
+                expect(response).to.have.property("status", 200)
+                expect(response.body).to.have.property("name", "Tokyo")
+                expect(response.body.sys).to.have.property("country", "JP")
+                expect(response.body).to.have.property("id", 1850147)
 
-        })
+            })
     })
 })
 
-/* describe("Pesquisa de Cidade Por ID Inválido", () => {
+ describe("Pesquisa de Cidade Por ID Inválido", () => {
     it("GET -READ", () => {
-        cy.request("GET", "api.openweathermap.org/data/2.5/weather?id=1850147&appid="
-        + Cypress.env("API_KEY")).then((response) => {
+        cy.request({
+            method: "GET",
+            url: "weather?q=Inexistente&appid="
+                + Cypress.env("API_KEY"),
+            failOnStatusCode: false
+        }).then((response) => {
 
             cy.log(JSON.stringify(response.body));
-            //expect(response).to.have.property("cod", 404)
-            //expect(response.body).to.have.property("message", "city not found")
+            expect(response.body).to.have.property("cod", "404")
+            expect(response.body).to.have.property("message", "city not found")
 
         })
     })
-}) */
+}) 
 
 // POR COORDENADAS GEOGRAFICAS
 
 describe("Pesquisa de Cidade Por Coordenadas Geográficas Válidas", () => {
-    it("GET -READ", () => {
-        cy.request("GET", "api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid="
-        + Cypress.env("API_KEY")).then((response) => {
-
-            cy.log(JSON.stringify(response.body));
-            expect(response).to.have.property("status", 200)
-            expect(response.body).to.have.property("name", "Shuzenji")
-            expect(response.body.sys).to.have.property("country", "JP")
-            expect(response.body).to.have.property("id", 1851632)
-            expect(response.body.coord).to.have.property("lon", 139)
-            expect(response.body.coord).to.have.property("lat", 35)
-
-
-
-        })
-    })
-})
-
-/* describe("Pesquisa de Cidade Por Coordenadas Geográficas Inválidas", () => {
     it("GET -READ", () => {
         cy.request("GET", "api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid="
             + Cypress.env("API_KEY")).then((response) => {
@@ -110,10 +101,26 @@ describe("Pesquisa de Cidade Por Coordenadas Geográficas Válidas", () => {
                 expect(response.body).to.have.property("name", "Shuzenji")
                 expect(response.body.sys).to.have.property("country", "JP")
                 expect(response.body).to.have.property("id", 1851632)
-
+                expect(response.body.coord).to.have.property("lon", 139)
+                expect(response.body.coord).to.have.property("lat", 35)
             })
     })
-}) */
+})
+
+ describe("Pesquisa de Cidade Por Coordenadas Geográficas Inválidas", () => {
+    it("GET -READ", () => {
+        cy.request({
+            method: "GET",
+            url: "weather?q=Inexistente&appid="
+                + Cypress.env("API_KEY"),
+            failOnStatusCode: false
+        }).then((response) => {
+                cy.log(JSON.stringify(response.body));
+            expect(response.body).to.have.property("cod", "404")
+            expect(response.body).to.have.property("message", "city not found")
+            })
+    })
+}) 
 
 // POR ZIP CODE
 
@@ -129,5 +136,20 @@ describe("Pesquisa de Cidade Por Zip Code Válido", () => {
                 expect(response.body.sys).to.have.property("country", "US")
 
             })
+    })
+})
+
+describe("Pesquisa de Cidade Por Zip Code Inválido", () => {
+    it("GET -READ", () => {
+        cy.request({
+            method: "GET",
+            url: "weather?q=Inexistente&appid="
+                + Cypress.env("API_KEY"),
+            failOnStatusCode: false
+        }).then((response) => {
+            cy.log(JSON.stringify(response.body));
+            expect(response.body).to.have.property("cod", "404")
+            expect(response.body).to.have.property("message", "city not found")
+        })
     })
 })
